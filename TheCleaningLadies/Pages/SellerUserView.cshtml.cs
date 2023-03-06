6712ccea510cc6c512c113dcb5195215a86f117c
Katie Knutson <71470339+katherynmk@users.noreply.github.com>
2023-03-06 14:42:49 -0600

    ok

---
+++ TheCleaningLadies/Pages/SellerUserView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesSellerUserView.Pages
+{
+    public class SellerUserViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
