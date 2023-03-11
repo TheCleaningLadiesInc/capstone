fb571d805c05aec2acce36ecc0482c4ae24bd93e
Katie Knutson <71470339+katherynmk@users.noreply.github.com>
2023-03-11 00:37:36 -0600

    sorta done

--- TheCleaningLadies/Pages/BuyerView.cshtml.cs
+++ TheCleaningLadies/Pages/BuyerView.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPagesBuyerView.Pages
+using RazorPagesUser.Models;
+using RazorPagesUser.Services;
+
+namespace RazorPagesUser.Pages
 {
     public class BuyerViewModel : PageModel
-    {
+    {   
+        public List<User> users = new();
+
+
         public void OnGet()
         {
+            //users = UserService.GetAll();
+            
+            
+            //NewUser = userService.Get("Test");
+            
         }
+
+        public void OnPost(string nlocation)
+        {
+            
+            UserService userService = new UserService();
+            users = userService.GetList(nlocation);
+        }
+
+        
     }
+
 }
